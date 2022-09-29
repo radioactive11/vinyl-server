6a6fb3e274008f8e2dcac5af9e1efd15dfc2682e
roy.arijit <arijit.roy@docsumo.com>
2022-09-29 16:33:44 +0530

    🔒️ `CORS` Allowed all domains

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 
 from app.api.v1.api import api_router
 
 app = FastAPI(title="vinyl")
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
-
-
